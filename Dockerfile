9313364b2416e1363806ecbb0b29f7a0ef727762
wender-gs <wendergs2@gmail.com>
2022-09-12 18:10:41 -0300

    Job from 09-12-2022

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu:latest
+
+RUN apt update && apt upgrade -y
+
+RUN apt install wget gnupg gnupg2 gnupg1 -y
+
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+RUN apt -y update
+
+RUN apt install google-chrome-stable git python3 python3-pip -y
+
+COPY . /app
+
+WORKDIR /app
+
+RUN pip install --upgrade pip
+
+RUN pip3 install -r requirements.txt
+
+CMD [ "python3", "app.py" ]
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+print("Iniciando serviços...")
 
 table = pd.read_excel('tp_data.xlsx', sheet_name=0)
 
@@ -10,43 +12,52 @@ df = pd.DataFrame(table)
 
 v = df['v']
 
-d = 0
+with open('dados.txt') as file:
+    try:
+        d = int(file.read())
+    except Exception as e:
+        d = int(0)
+        print(e)
 
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 
-driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
+print("instanciando o browser...")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+print("Inserindo registros se disponiveis...")
 
 for i in v:  # variavel i retorna os valores contidos na coluna v
 
     while i > d:
-
         with open("dados.txt", "r") as arquivo:
             ac: str = arquivo.read()
 
-        if str(ac) < str(i):
-            # entrando na página de cadastro
-            driver.get("http://localhost:8000/usuarios/create")
+        # entrando na página de cadastro
+        driver.get("http://localhost:8000/usuarios/create")
 
-            # recuperando o input do username e adicionando o valor a ele
-            username = driver.find_element('xpath', '//*[@id="name"]')
-            username.send_keys(df.loc[d]['username'])
+        # recuperando o input do username e adicionando o valor a ele
+        username = driver.find_element('xpath', '//*[@id="name"]')
+        username.send_keys(df.loc[d]['username'])
 
-            # recuperando o input do email e adicionando o valor a ele
-            email = driver.find_element('xpath', '//*[@id="email"]')
-            email.send_keys(df.loc[d]['email'])
+        # recuperando o input do email e adicionando o valor a ele
+        email = driver.find_element('xpath', '//*[@id="email"]')
+        email.send_keys(df.loc[d]['email'])
 
-            # recuperando o input do password e adicionando o valor a ele
-            password = driver.find_element('xpath', '//*[@id="pass"]')
-            password.send_keys(str(df.loc[d]['password']))
+        # recuperando o input do password e adicionando o valor a ele
+        password = driver.find_element('xpath', '//*[@id="pass"]')
+        password.send_keys(str(df.loc[d]['password']))
 
-            # clicando no botão de cadastro
-            driver.find_element('xpath', '/html/body/section/div[2]/form/button').click()
+        # clicando no botão de cadastro
+        driver.find_element('xpath', '/html/body/section/div[2]/form/button').click()
 
-            with open("dados.txt", 'w') as arquivo:
-                arquivo.write(str(i))
+        with open("dados.txt", 'w') as arquivo:
+            arquivo.write(str(i))
 
         d += 1
 
 driver.quit()
+
+print("atualização concluida!")
